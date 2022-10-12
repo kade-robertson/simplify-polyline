2ff0c8534c467651aa4e3f2871ff85b1bb5ab7f2
Kade Robertson <kade@kaderobertson.dev>
2022-10-12 09:23:58 -0400

    test: update tests + benchmarking

--- benches/bench.rs
+++ benches/bench.rs
@@ -9,48 +9,48 @@ pub const BENCH_FIXTURE_73752: &str = include_str!("../fixtures/bench-73752.json
 
 #[bench]
 fn simplify_hq_1118_pts(b: &mut Bencher) {
-    let points = serde_json::from_str::<Vec<Point>>(BENCH_FIXTURE_1118).unwrap();
+    let points = serde_json::from_str::<Vec<Point<f64>>>(BENCH_FIXTURE_1118).unwrap();
     b.iter(|| simplify(&points, 1.0, true));
 }
 
 #[bench]
 fn simplify_lq_1118_pts(b: &mut Bencher) {
-    let points = serde_json::from_str::<Vec<Point>>(BENCH_FIXTURE_1118).unwrap();
+    let points = serde_json::from_str::<Vec<Point<f64>>>(BENCH_FIXTURE_1118).unwrap();
     b.iter(|| simplify(&points, 1.0, false));
 }
 
 #[bench]
 fn simplify_hq_1118_pts_tol5(b: &mut Bencher) {
-    let points = serde_json::from_str::<Vec<Point>>(BENCH_FIXTURE_1118).unwrap();
+    let points = serde_json::from_str::<Vec<Point<f64>>>(BENCH_FIXTURE_1118).unwrap();
     b.iter(|| simplify(&points, 5.0, true));
 }
 
 #[bench]
 fn simplify_lq_1118_pts_tol5(b: &mut Bencher) {
-    let points = serde_json::from_str::<Vec<Point>>(BENCH_FIXTURE_1118).unwrap();
+    let points = serde_json::from_str::<Vec<Point<f64>>>(BENCH_FIXTURE_1118).unwrap();
     b.iter(|| simplify(&points, 5.0, false));
 }
 
 #[bench]
 fn simplify_hq_73752_pts(b: &mut Bencher) {
-    let points = serde_json::from_str::<Vec<Point>>(BENCH_FIXTURE_73752).unwrap();
+    let points = serde_json::from_str::<Vec<Point<f64>>>(BENCH_FIXTURE_73752).unwrap();
     b.iter(|| simplify(&points, 1.0, true));
 }
 
 #[bench]
 fn simplify_lq_73752_pts(b: &mut Bencher) {
-    let points = serde_json::from_str::<Vec<Point>>(BENCH_FIXTURE_73752).unwrap();
+    let points = serde_json::from_str::<Vec<Point<f64>>>(BENCH_FIXTURE_73752).unwrap();
     b.iter(|| simplify(&points, 1.0, false));
 }
 
 #[bench]
 fn simplify_hq_73752_pts_tol5(b: &mut Bencher) {
-    let points = serde_json::from_str::<Vec<Point>>(BENCH_FIXTURE_73752).unwrap();
+    let points = serde_json::from_str::<Vec<Point<f64>>>(BENCH_FIXTURE_73752).unwrap();
     b.iter(|| simplify(&points, 5.0, true));
 }
 
 #[bench]
 fn simplify_lq_73752_pts_tol5(b: &mut Bencher) {
-    let points = serde_json::from_str::<Vec<Point>>(BENCH_FIXTURE_73752).unwrap();
+    let points = serde_json::from_str::<Vec<Point<f64>>>(BENCH_FIXTURE_73752).unwrap();
     b.iter(|| simplify(&points, 5.0, false));
 }
--- tests/integration.rs
+++ tests/integration.rs
@@ -2,22 +2,22 @@ use simplify_polyline::{point, points, simplify, Point};
 
 #[test]
 fn returns_empty_vec_if_no_points() {
-    let result = simplify(&[], 1.0, false);
+    let result = simplify::<f64>(&[], 1.0, false);
     assert_eq!(result.len(), 0);
 }
 
 #[test]
 fn returns_empty_vec_if_one_point() {
-    let input = points!((0.0, 0.0));
+    let input = points!(f64, (0.0, 0.0));
     let result = simplify(input, 1.0, false);
     assert_eq!(result[0], input[0]);
 }
 
 #[test]
 fn matches_expected_output() {
-    let input = serde_json::from_str::<Vec<Point>>(include_str!("../fixtures/test-case.json"));
+    let input = serde_json::from_str::<Vec<Point<f64>>>(include_str!("../fixtures/test-case.json"));
     let expected_output =
-        serde_json::from_str::<Vec<Point>>(include_str!("../fixtures/test-case-output.json"));
+        serde_json::from_str::<Vec<Point<f64>>>(include_str!("../fixtures/test-case-output.json"));
 
     assert!(input.is_ok());
     assert!(expected_output.is_ok());
